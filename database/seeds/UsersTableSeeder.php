7e048253f222882828d20231597d7f5e0f4350ca
Alice <alicenjoroge707@gmail.com>
2020-01-06 08:09:26 +0300

    Invite Users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user=[
+            'name' => 'AdminUser',
+            'email'=> 'user@example.com',
+            'password'=> bcrypt('password')
+        ];
+        User::create($user);
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/users', 'UsersController@index')->name('users');
+Route::get('/users/invite', 'UsersController@invite_view')->name('invite_view');
+Route::post('/users/invite', 'UsersController@process_invites')->name('process_invite');
+Route::get('/registration/{token}', 'UsersController@registration_view')->name('registration');
+Route::POST('/registration', 'Auth\RegisterController@register')->name('accept');
