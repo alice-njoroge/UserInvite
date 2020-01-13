de1c8a4e5893f5e6276e42284117bb8cdb678b0a
Alice <alicenjoroge707@gmail.com>
2020-01-13 12:48:30 +0300

    add permissions view on create role page

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolesController extends Controller
@@ -15,7 +16,8 @@ class RolesController extends Controller
 
     public function create()
     {
-        return view('pages.roles.create');
+        $permissions = Permission::all();
+        return view('pages.roles.create',['permissions'=>$permissions]);
 
     }
 }
--- resources/views/pages/roles/create.blade.php
+++ resources/views/pages/roles/create.blade.php
@@ -5,24 +5,27 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Create A New Role</div>
+                    <div class="card-header">Define Name and permissions for the Role</div>
                     <div class="card-body">
                         <form>
                             <div class="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                <label for="text">Name</label>
+                                <input type="text" class="form-control" placeholder="Enter role name">
                             </div>
                             <div class="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                @foreach($permissions as $permission)
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                        <label class="custom-control-label" for="customCheck1">
+                                            {{$permission->name}}</label>
+                                    </div>
+                                @endforeach
                             </div>
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
+
                         </form>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary float-right">Submit</button>
                     </div>
                 </div>
             </div>
